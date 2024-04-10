2511dd6c5c84e599e272b319844cc6952e37837d
PatrickPereyra12 <109742438+PatrickPereyra12@users.noreply.github.com>
2024-04-09 21:10:20 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Hola a todos amigos!</h1>'
 @app.route('/user/<name>')
 def user(name):
     return '<h1>bienvenido, %s!</h1>' % name
